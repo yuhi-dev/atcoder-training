afc476e5f0268de01995a23f9fc49fbb203458bb
yuhi-dev <yuhiro.askabe@gmail.com>
2024-03-23 13:24:36 +0900

    commite template

--- .vscode/template.cpp
+++ .vscode/template.cpp
@@ -3,8 +3,11 @@ using namespace std;
 #include <atcoder/all>
 using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n+1); i++)
 using ll = long long;
 using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using mint = modint1000000007;
 
 int main() {
   int n;
