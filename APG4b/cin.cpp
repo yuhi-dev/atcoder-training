d675d52fd6805e56b2a6b733f0604049cf831bcc
yuhi-dev <yuhiro.askabe@gmail.com>
2024-03-24 17:22:44 +0900

    update

---
+++ APG4b/cin.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // 一年の秒数
+  int seconds = 365 * 24 * 60 * 60;
+
+  // 以下のコメントを消して追記する
+  cout << seconds << endl;
+  cout << seconds * 2 << endl;
+  cout << seconds * 5 << endl;
+  cout << seconds * 10 << endl;
+}
